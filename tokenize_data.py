9282a984c5e23345665c6d23c313f40d1dbd3295
User <nevo.krien@gmail.com>
2023-07-04 15:36:24 +0300

    fixed the sampeling to be as needed

--- tokenize_data.py
+++ tokenize_data.py
@@ -7,6 +7,7 @@ import os
 import json
 import tempfile 
 import hashlib
+from collections import defaultdict
 
 import numpy as np
 from transformers import GPT2Tokenizer
@@ -18,35 +19,49 @@ def get_hash(code):
     return hash.hexdigest()
 
 def load_data(file):
-    data = {}
+    data = defaultdict(lambda:{})
     errors = 0
     with open(file,'rb') as f:
         for i,line in enumerate(f):
             try:
-                t = json.loads(line)['content']
+                repo=json.loads(line)
+                t = repo['content']
                 k = get_hash(t)
-                data[k] = t
+                #data[k] = t
+                entry=data[k]
+                if len(entry)==0:
+                    entry.update({'text':t,'repos':[repo['repo_name']]})
+                else: 
+                    entry['repos'].append(repo['repo_name'])
             except Exception as e:
                 print(f'Errored on file {file} at line {i}: {e}')
                 errors += 1
-    return data, errors
+    return dict(data), errors
 
-def load_all_data(dir_path, num_workers: int = None):
+def load_all_data(dir_path,save_path, num_workers: int = None):
     num_workers = num_workers if num_workers else cpu_count()
     
     print("Starting loading data...")
     with Pool(num_workers) as p:
         results = p.map(load_data, [os.path.join(dir_path, file) for file in os.listdir(dir_path) if file.endswith('.json')])
     print("Finished loading data.")
-    
-    data = {}
+
+    data = defaultdict(lambda:{})
     total_errors = 0
     for result in results:
-        data.update(result[0])
         total_errors += result[1]
+        #updating the values
+        for k,v in result[0].items():
+            entry=data[k]
+            if len(entry)==0:
+                entry.update(v)
+            else: 
+                entry['repos'].extend(v['repos'])
+
+
     print(f"Loaded data from {len(results)} files with a total of {total_errors} errors.")
-    
-    return list(data.values())
+
+    return [x['text'] for x in data.values()],[x['repos'] for x in data.values()]
 
 def get_mem_usage(code):
     '''
@@ -82,38 +97,62 @@ def tokenize_text(params):
 
 
 def preprocess_data(dir_path, save_path, tokenizer_path, max_len, gpt_cut, mem_cut, test_size,debug_cut_size):
+    assert os.path.exists(save_path) 
+
     tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
-    codes = load_all_data(dir_path)
+    codes,names = load_all_data(dir_path,save_path)
 
     if debug_cut_size!=None:
         codes=codes[:debug_cut_size]
+        names=names[:debug_cut_size]
         print(f'cuted codes to length{len(codes)}')
+
 
     print("Starting tokenization...")
     with Pool(cpu_count()) as p:
         token_chunks = p.map(tokenize_text, [(text, tokenizer, max_len, gpt_cut, mem_cut) for text in codes])
 
-    # Filter out None results from tokenization
-    tokens = []
-    for chunks in token_chunks:
-        if chunks is not None:  # Filter out None results
-            for chunk in chunks:
-                tokens.append(chunk)  # Flatten the list
-    print(f"Finished tokenization. Kept {len(tokens)} sequences.")
-    # Convert to numpy array
-    tokens = np.array(tokens)
+    names=[name for name,chunks in zip(names,token_chunks) if chunks is not None]
+    token_chunks=[chunks for chunks in token_chunks if chunks is not None]
+    print(f"Finished tokenization. Kept {len(token_chunks)} files.")
 
+    print('Spliting the dataset')
     # Split into train and test sets
-    indices = np.arange(len(tokens))
+    indices = np.arange(len(token_chunks))
     test_indices = np.random.choice(indices, size=test_size, replace=False)
     train_indices = np.array(list(set(indices) - set(test_indices)))
 
-    train_tokens = tokens[train_indices]
-    test_tokens = tokens[test_indices]
+    train_chunks=[token_chunks[i] for i in train_indices]
+    train_names=[names[i] for i in train_indices]
 
-    # Save to numpy files
+    test_chunks=[token_chunks[i] for i in test_indices]
+    test_names=[names[i] for i in test_indices]
+    
+    print('Saving repo names')
+    with open(os.path.join(save_path,'train_names.json'), 'w') as file:
+        json.dump(train_names, file)
+    with open(os.path.join(save_path,'test_names.json'), 'w') as file:
+        json.dump(test_names, file)
+
+    #flattening the chunks 
+    train_tokens = []
+    for chunks in train_chunks:
+        for chunk in chunks:
+            train_tokens.append(chunk)  
+
+    train_tokens=np.array(train_tokens) 
     np.save(os.path.join(save_path, "train_tokens.npy"), train_tokens)
+    print(f"train ready with {len(train_tokens)} sequnces")
+    
+    test_tokens = []
+    for chunks in test_chunks:
+        for chunk in chunks:
+            test_tokens.append(chunk)  
+    
+    test_tokens=np.array(test_tokens) 
     np.save(os.path.join(save_path, "test_tokens.npy"), test_tokens)
+    print(f"test ready with {len(test_tokens)} sequnces")
+
 
 if __name__ == "__main__":
     import argparse
